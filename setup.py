33c94101c321be0b38627d4b81a47a18ff10250f
Chris FItz <christopher.fitz07@gmail.com>
2022-06-15 11:59:38 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='caftools',
+        version='0.1',
+        description='A collection of functions to analyze MS',
+        url='http://github.com/chrisfitz/caftools',
+        author='Chris Fitz',
+        author_email='christopher.fitz07@gmail.com',
+        license='MIT',
+        packages=['caf'],
+        zip_safe=False)
