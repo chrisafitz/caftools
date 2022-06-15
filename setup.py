c7e7780669bcf22efb7c8cb0bb109e61e7e9ab83
chrisafitz <christopher.fitz07@gmail.com>
2022-06-15 15:43:10 -0500

    change type

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(name='caftools',
         version='0.1',
-        description='A collection of functions to analyze MS',
+        description='A collection of functions to analyze MD',
         url='http://github.com/chrisfitz/caftools',
         author='Chris Fitz',
         author_email='christopher.fitz07@gmail.com',
         license='MIT',
-        packages=['caf'],
+        packages=['caftools'],
         zip_safe=False)
