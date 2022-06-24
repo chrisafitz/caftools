41caa872fec99f543fd4b4ff3c97acf711878825
chrisafitz <christopher.fitz07@gmail.com>
2022-06-24 12:27:38 -0500

    '

--- caftools/transport/analysis.py
+++ caftools/transport/analysis.py
@@ -339,7 +339,7 @@ def density():
     dens_l = kdtree(index, cutoff, vol, my_l, pmol)
     print("First done...")
 
-    print(type(dens_l))
+    print(len(dens_l))
     print(dens_l)
     average = ([np.mean(np.array(x)) for x in dens_l])
     avg = round(np.mean(np.array(average)), 2)
