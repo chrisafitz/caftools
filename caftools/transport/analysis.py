6ebf453a044e27b5f264aadb82469b935801cca2
chrisafitz <christopher.fitz07@gmail.com>
2022-06-30 14:49:31 -0500

    up

--- caftools/transport/analysis.py
+++ caftools/transport/analysis.py
@@ -114,16 +114,18 @@ def msd():
         name = "Christopher_2022"
         np.savetxt( 'msd-{}-overall-{}.txt'.format(mol, name),np.transpose(np.vstack([trj.time, MSD])),header='# Time (ps)\tMSD (nm^2)')
         tempe = 298 #write the temperature
+        res = stats.linregress(trj.time,MSD)
+        fig, ax = plt.subplots()
+        ax.plot(trj.time, MSD)
+        ax.plot(trj.time, res.intercept + res.slope*(trj.time), 'r', alpha=0.3, linewidth= 0.8)
         
         # stats from end of sim
         chunks = 5
         chunk = len(MSD)//5
         chunked_MSD = MSD[2*chunk:]
+        chunked_trj= trj[2*chunk:]
+        chunked_res = stats.linregress(chunked_trj.time,chunked_MSD)
         
-        res = stats.linregress(trj.time, chunked_MSD)
-        fig, ax = plt.subplots()
-        ax.plot(trj.time, MSD)
-        ax.plot(trj.time, res.intercept + res.slope*(trj.time), 'r', alpha=0.3, linewidth= 0.8)
         slope = '{:.2e}'.format(res.slope)
         dif_c = '{:.2e}'.format((res.slope)*(1/(1*(10**18)))*(1/6)*(1*(10**12)))
         stddev = '{:.2e}'.format(stdev)
