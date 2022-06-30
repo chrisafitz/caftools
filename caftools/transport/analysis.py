c05364a2da5a5b8a38e054fb9dd357860ebd03d5
chrisafitz <christopher.fitz07@gmail.com>
2022-06-30 14:07:12 -0500

    25

--- caftools/transport/analysis.py
+++ caftools/transport/analysis.py
@@ -90,6 +90,9 @@ def msd():
 
     def _run_overall(trj, mol):
         D, MSD, x_fit, y_fit = calc_msd(trj)
+        print(MSD)
+        print(len(MSD))
+        
         return D, MSD
     
     
@@ -114,7 +117,6 @@ def msd():
         np.savetxt( 'msd-{}-overall-{}.txt'.format(mol, name),np.transpose(np.vstack([trj.time, MSD])),header='# Time (ps)\tMSD (nm^2)')
         tempe = 298 #write the temperature
         res = stats.linregress(trj.time, MSD)
-        print(res)
         fig, ax = plt.subplots()
         ax.plot(trj.time, MSD)
         ax.plot(trj.time, res.intercept + res.slope*(trj.time), 'r', alpha=0.3, linewidth= 0.8)
