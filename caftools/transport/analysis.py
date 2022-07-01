dbdda083af1c8ff670fa843eee78baf1d74566e2
chrisafitz <christopher.fitz07@gmail.com>
2022-07-01 14:29:57 -0500

    updates

--- caftools/transport/analysis.py
+++ caftools/transport/analysis.py
@@ -34,55 +34,29 @@ workspace_path = '{}/workspace'.format(base_path)
 ### Unwrapping
 def unwrap():
 
-    def make_comtrj(trj):
-        """Takes a trj and returns a trj with COM positions as atoms"""
-
-        comtop = md.Topology()
-        coords = np.ndarray(shape=(trj.n_frames, trj.n_residues, 3))
-
-        for j, res in enumerate(trj.topology.residues):
-            comtop.add_atom(res.name, virtual_site, comtop.add_residue(res.name, comtop.add_chain()))
-            res_frame = trj.atom_slice([at.index for at in res.atoms])
-            coords[:, j, :] = md.compute_center_of_mass(res_frame)
-
-        comtrj = md.Trajectory(xyz=coords,
-                            topology=comtop,
-                            time=trj.time,
-                            unitcell_angles=trj.unitcell_angles,
-                            unitcell_lengths=trj.unitcell_lengths)
-
-        return comtrj
-
     xtc_file = ('sample.xtc')
     gro_file = ('sample.gro')
     tpr_file = ('sample.tpr')
     if os.path.isfile(xtc_file) and os.path.isfile(gro_file):
-        print('huh')
+
         os.system('echo 0 | gmx trjconv -f {0} -o {1} -s {2} -skip 10 -pbc nojump'.format(xtc_file, 'sample_unwrapped.xtc', tpr_file))
         unwrapped_trj = ('sample_unwrapped.xtc')
         
-        os.system('echo 0 | gmx trjconv -f {0} -o {1} -s {2} -skip 10 -pbc res'.format(xtc_file, 'sample_res.xtc', tpr_file))
-        res_trj = ('sample_res.xtc')
         
-        '''
-        trj1 = md.load(res_trj, top=gro_file)
-        trj2= md.load(unwrapped_trj, top=gro_file)
-        comtrj = make_comtrj(trj2)
-        comtrj.save_xtc('unwrapped_com_trj')
+        os.system('echo 0 | gmx trjconv -f {0} -o {1} -s {2} -skip 10 -pbc res'.format(xtc_file, 'sample_res.xtc', tpr_file))
+        res_trj = 'sample_res.xtc'
+        trj = md.load(res_trj, top=gro_file)
+        trj = md.load(unwrapped_trj, top=gro_file)
+        comtrj = make_comtrj(trj)
+        comtrj.save_xtc('sample_com_unwrapped.xtc')
         comtrj[-1].save_gro('com.gro')
         print('make whole')
-        '''
-        
         os.system('echo 0 | gmx trjconv -f {0} -o {1} -s {2} -skip 10 -pbc whole'.format(xtc_file, 'sample_whole.xtc', gro_file))
         whole_trj = ('sample_whole.xtc')
-        whole_com_trj =('sample_com_whole.xtc')
         trj_whole = md.load(whole_trj, top=gro_file)
-        print(trj_whole)
         trj_whole_com = make_comtrj(trj_whole)
-        trj_whole_com[-1].save_gro('com.gro')
         print('saving')
-        trj_whole_com.save_xtc(whole_com_trj)
-        print(trj_whole_com)
+        trj_whole_com.save_xtc('sample_com_whole.xtc')
  
  
 ### Mean Squared Displacement
