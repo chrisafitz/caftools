5c38ceafc42aa8d272cd8fb8701951a483c69bb0
chrisafitz <christopher.fitz07@gmail.com>
2022-07-13 14:20:34 -0500

    up

--- caftools/transport/analysis.py
+++ caftools/transport/analysis.py
@@ -133,8 +133,10 @@ def msd():
 
 
     print('Loading trj ')
-    top_file = ('com.gro')
-    trj_file = ('sample_com_unwrapped.xtc')
+    ##top_file = ('com.gro')
+    ##trj_file = ('sample_com_unwrapped.xtc')
+    top_file = ('sample.gro')
+    trj_file = ('sample.trj')
     trj = md.load(trj_file,top=top_file)
     temp = 298  #write temperature as number  298
 
